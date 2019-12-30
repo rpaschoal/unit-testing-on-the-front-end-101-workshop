022d1bc9ac599634e9e3cd3a81f6490d0e102f1b
Rafael Carvalho <rpaschoal@live.com>
2019-12-31 11:12:07 +1300

    Add rudimentar TodoList component to be used as an example

---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,67 @@
+// Very simple and rudimentar todo component for a simple unit testing on the front end workshop (101)
+
+import React, { useState, useRef } from 'react'
+
+import styles from './Todo.module.scss' ;
+
+interface ITodoItemProps {
+    itemText: string;
+    onRemove: (itemText: string) => void;
+}
+
+const TodoItem: React.FC<ITodoItemProps> = props => {
+    return (
+        <li>
+            {props.itemText} <a href="#" onClick={() => props.onRemove(props.itemText)}>(Remove)</a>
+        </li>
+    );
+};
+
+const TodoList: React.FC = () => {
+    const [todoItems, setTodoItems] = useState<string[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const addTodoItem = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        if (inputRef.current)
+        {
+            const itemText = inputRef.current.value;
+
+            if (itemText.trim().length > 0 && todoItems.findIndex(t => t === itemText) < 0)
+            {
+                setTodoItems([...todoItems, itemText]);
+            }
+
+            inputRef.current.value = '';
+        }
+    }
+
+    const removeTodoItem = (itemText: string) => {
+        const indexToRemove = todoItems.findIndex(t => t === itemText);
+
+        if (indexToRemove >= 0)
+            setTodoItems([...todoItems.slice(0, indexToRemove), ...todoItems.slice(indexToRemove + 1)]);
+    }
+
+    return (
+        <div className={styles["todo-list"]}>
+            <div className={styles["todo-list-header"]}>
+                <form onSubmit={addTodoItem}>
+                    <input placeholder="Enter your Task" ref={inputRef} required/>
+                    <button type="submit" >Add Task</button>
+                </form>
+            </div>
+            <ul className={styles["todo-list-items"]}>
+                {todoItems.map((item, index) => {
+                    return <TodoItem key={index} itemText={item} onRemove={removeTodoItem} />
+                })}
+            </ul>
+        </div>
+    );
+};
+
+
+
+export default TodoList
+
